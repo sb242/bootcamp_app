474b2a932a6e3fc54a8dbc1fa307289fb045e6d5
Spencer <spencerb242@proton.me>
2022-09-21 22:56:30 -0600

    add pg to our app to allow database querying

---
+++ teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'spencerbethel',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+
+pool.query(`
+SELECT DISTINCT teachers.name as name, cohorts.name as cohort
+FROM assistance_requests 
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = '${cohortName}'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach((teacher) => {
+    console.log(`${teacher.cohort}: ${teacher.name}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
